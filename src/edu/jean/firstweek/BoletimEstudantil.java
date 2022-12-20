46001608c8706ec2e6833dcf991ca972c4ad7122
jeanspaixao <jeanspaixao@gmail.com>
2022-12-20 13:36:46 -0300

    creating folder structure

---
+++ src/edu/jean/firstweek/BoletimEstudantil.java
@@ -0,0 +1,10 @@
+package edu.jean.firstweek;
+public class BoletimEstudantil {
+    public static void main(String[] args){
+        int mediaFinal = 7;
+        if  (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else
+            System.out.println("APROVADO");
+    }
+}
