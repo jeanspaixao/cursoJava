1b213510e7ae09e45c960999b3d02e048aa5016b
jeanspaixao <jeanspaixao@gmail.com>
2022-12-20 13:32:34 -0300

    adding BoletimEstudantil

---
+++ src/BoletimEstudantil.java
@@ -0,0 +1,9 @@
+public class BoletimEstudantil {
+    public static void main(String[] args){
+        int mediaFinal = 7;
+        if  (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else
+            System.out.println("APROVADO");
+    }
+}
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,13 @@
 public class MyClass {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    String primeiroNome = "Gleyson";
+    String segundoNome = "Sampaio";
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
